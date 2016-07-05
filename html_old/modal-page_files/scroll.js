b163b90e037f9982b1d156c8df649085e98fabe1
crazyyy <crazyyy@gmail.com>
2016-07-05 23:27:54 +0300

    start work

--- html_old/modal-page_files/scroll.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("._scollable").click(function(event){event.preventDefault();var dest=0;if($(this.hash).offset().top>$(document).height()-$(window).height()){dest=$(document).height()-$(window).height()-50;}else{dest=$(this.hash).offset().top-50;}$('html,body').animate({scrollTop:dest},1000,'swing');});});
